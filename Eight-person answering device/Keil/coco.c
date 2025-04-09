745c165b1df23322d4926e847e5ec1820fb66e75
wangjieWJie <ax15605438760@foxmail.com>
2025-04-09 17:12:32 +0800

    基本完成

--- Eight-person answering device/Keil/coco.c
+++ Eight-person answering device/Keil/coco.c
@@ -12,6 +12,7 @@ void delay(uint ms);
 sbit BB = P2^4;   //蜂鸣器
 sbit LED = P2^5;	
 sbit ctrl = P2^7;	//主持人总控键
+sbit P2_6 = P2^6;
 /* sfr button = P3;		选手抢答键
 	 sfr display = P0;	八段数码管 */
 sbit select1 = P2^0;	//数码管选段1,用于显示选中的选手编号
@@ -20,11 +21,13 @@ sbit select3 = P2^2;	//数码管选段3,
 sbit select4 = P2^3;	//数码管选段4,
 
 uchar P3is; //记录一瞬间的按键
-uchar num_seg[10] = {0X3F,0X06,0X5B,0X4F,0X66,0X6D,0X7D,0X07,0X7F,0X6F};  //数码管共阴极字段码
+uchar num_seg[12] = {0X3F,0X06,0X5B,0X4F,0X66,0X6D,0X7D,0X07,0X7F,0X6F,0X77,0X7C};  //数码管共阴极字段码
 uchar disp_now = 0X88;
 uint time_counter = 0;   //定时器的计数器，每记20次表示过了1秒（50ms*20 = 1s）
 uint BBS = 1;  // 表示蜂鸣器响多少秒，只要不为零就响，无需多余操作
 uint gogogo = 0; //表示数码管倒计时多少秒，只要不为零就计时，无需多余操作
+uint outwhile = 0;
+
 
 void main()
 {
@@ -35,49 +38,83 @@ void main()
 		if(!ctrl)  // 主持人按下按钮进入抢答
 		{
 			BBS = 1;
-			while(1)
+			BB = 1;  //可以不写这句话，但是不写的话蜂鸣器肯定会延时响应
+			while(1)	
 			{
-				if(P3 != 0XFF && disp_now == 0X88) //某个按钮按下时进入循环，检测哪个按钮被按下,我的意思是一次循环那么快，就算两个按钮同时按下，也有先后顺序吧，也能检测到吧（Doge）
+				if(outwhile == 1)
+				{
+					outwhile = 0;
+					P2_6 = 0;
+					break;
+				}
+				
+				if(P3 != 0XFF && disp_now == 0X88) //某个按钮按下时进入，检测哪个按钮被按下,我的意思是一次循环那么快，就算两个按钮同时按下，也有先后顺序吧，也能检测到吧（Doge）
 				{
 					P3is = P3;
-					easy_button();	//检测按下的案件，存入disp_now中，此行可保证这个条件语句只被执行一次
+					easy_button();	//检测按下的按键，存入disp_now中，此行可保证这个条件语句只被执行一次
 					display(disp_now, 1); //检测完按键按下直接显示
-					BBS = 2;	//蜂鸣器响两秒
+					BBS = 1;	//蜂鸣器响两秒
+					BB = 1;  //可以不写这句话，但是不写的话蜂鸣器肯定会延时响应
 					gogogo = 10; //十秒倒计时
 				}
 				else if(disp_now != 0X88) // 表示按键按下并记录完成，进入下一步，开始十秒倒计时
 				{
-					//display(disp_now, 1);      
-					if(gogogo > 0 && gogogo <= 10)
+					while(1)
 					{
-						display(num_seg[gogogo-1],2);
-					}
-					else
-					{
-						BBS = 2;
-						disp_now = 0X88;
-						break;
+						display(disp_now, 1);
+
+						if(gogogo > 0 && gogogo <= 10)
+						{
+							display(num_seg[gogogo-1],4);
+						}
+						else
+						{
+							BBS = 1;
+							BB = 1;  //可以不写这句话，但是不写的话蜂鸣器肯定会延时响应
+							disp_now = 0X88;
+							outwhile = 1;
+							break;
+						}
 					}
 				}
-				//delay(500);
 			}
 		}
 	}
 }
 
 
+void Timer0_ISR() interrupt 1   // 定时器0的中断 ,模式2无需手动重载TH0/TL0
+{
+    time_counter++;             // 累计中断次数
+    
+    if(time_counter >= 10000)      // 10000次×100μs=1000ms
+    {
+      time_counter = 0;
+			if(BBS > 0)
+			{
+				BB = 1; //蜂鸣器BB响
+				BBS--; //每秒减一
+			}
+			else
+			{
+				BB = 0; 
+			}
+				
+			if(gogogo > 0 )  //倒计时
+			{
+				gogogo--;
+			}
+		}
+}
 
 
 
 void delay(uint ms)
 {
-	uint i,j;
-	for(i = 52;i>0;i--)
-	{
-		for(j=ms;j>0;j--)
-		{
-		}
-	}
+	int i, j;
+	for(; ms > 0; ms--)
+		for (i = 2; i > 0; i--)
+       for (j = 239; j > 0; j--);
 }
 
 
@@ -90,20 +127,9 @@ void display(uchar dispN, uint S)	//数码管显示，参数为显示数字和显示位置
 			select2 = 1;
 			select3 = 1;
 			select4 = 1;
-			P0 = dispN;
-			select1 = 1;
-			select2 = 1;
-			select3 = 1;
-			select4 = 1;
-			break;
 		case 2:
 			select1 = 1;
 			select2 = 0;
-			select3 = 1;
-			select4 = 1;
-			P0 = dispN;
-			select1 = 1;
-			select2 = 1;
 			select3 = 1;
 			select4 = 1;
 			break;
@@ -112,25 +138,22 @@ void display(uchar dispN, uint S)	//数码管显示，参数为显示数字和显示位置
 			select2 = 1;
 			select3 = 0;
 			select4 = 1;
-			P0 = dispN;
-			select1 = 1;
-			select2 = 1;
-			select3 = 1;
-			select4 = 1;
 			break;
 		case 4:
 			select1 = 1;
 			select2 = 1;
 			select3 = 1;
-			select4 = 0;
-			P0 = dispN;
-			select1 = 1;
-			select2 = 1;
-			select3 = 1;
-			select4 = 1;
+			select4 = 0;	
 			break;
-		delay(1);
 	}
+	P0 = dispN;
+		
+	delay(3);
+				
+	select1 = 1;
+	select2 = 1;
+	select3 = 1;
+	select4 = 1;
 }
 
 
@@ -178,29 +201,6 @@ void int_timer0()   // 初始化定时器0为模式2（自动重载）
     EA = 1;         // 开启全局中断
 }
 
-void Timer0_ISR() interrupt 1   // 定时器0的中断 ,模式2无需手动重载TH0/TL0
-{
-    time_counter++;             // 累计中断次数
-    
-    if(time_counter >= 10000)      // 10000次×100μs=1000ms
-    {
-      time_counter = 0;
-			if(BBS > 0)
-			{
-				BB = 1; //蜂鸣器BB响
-				BBS--; //每秒减一
-			}
-			else
-			{
-				BB = 0; 
-			}
-				
-			if(gogogo > 0 )  //倒计时
-			{
-				gogogo--;
-			}
-		}
-}
 		
 
 
